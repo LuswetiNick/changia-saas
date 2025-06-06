460c9c50c44467f6d6e0e880bc884e32dc5ada88
LuswetiNick <nicklusweti@protonmail.com>
2025-06-06 18:44:28 +0300

    🔖 Add home layout

---
+++ src/lib/index.ts
@@ -0,0 +1,31 @@
+export const NavLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "#about",
+  },
+  {
+    name: "How It Works",
+    href: "#how-it-works",
+  },
+  {
+    name: "Featured",
+    href: "#featured",
+  },
+  {
+    name: "Pricing",
+    href: "#pricing",
+  },
+
+  {
+    name: "Testimonials",
+    href: "#testimonials",
+  },
+  {
+    name: "FAQ",
+    href: "#faq",
+  },
+];
